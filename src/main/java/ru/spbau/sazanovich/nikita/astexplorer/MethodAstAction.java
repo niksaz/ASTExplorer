e550cf340cccb5e4c0cdc115817477861637c6df
niksaz <nikitasazanovich@gmail.com>
2017-09-17 12:54:10 +0200

    Tracing of the current method implemented.

--- src/main/java/ru/spbau/sazanovich/nikita/astexplorer/MethodAstAction.java
+++ src/main/java/ru/spbau/sazanovich/nikita/astexplorer/MethodAstAction.java
@@ -1,47 +1,78 @@
 package ru.spbau.sazanovich.nikita.astexplorer;
 
-import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Strings;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.LangDataKeys;
-import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.impl.source.PsiMethodImpl;
+import org.jetbrains.annotations.Nullable;
 
 public class MethodAstAction extends AnAction {
 
   private static final int INDENTION_SPACES_PER_LEVEL = 2;
 
   public void actionPerformed(AnActionEvent event) {
-    Project project = event.getData(PlatformDataKeys.PROJECT);
-    PsiElement psiElement = event.getData(LangDataKeys.PSI_ELEMENT);
-    StringBuilder stringBuilder = new StringBuilder();
-    traversePsiElement(psiElement, stringBuilder, 0);
-    Messages.showMessageDialog(
-        project,
-        stringBuilder.toString(),
-        "Information",
-        Messages.getInformationIcon());
+    Project project = ActionEventUtil.getProjectFor(event);
+    Editor editor = ActionEventUtil.getEditorFor(event);
+    if (editor == null) {
+      showInfoMessage("Editor is not selected", project);
+      return;
+    }
+    PsiFile psiFile = ActionEventUtil.getPsiFileFor(event);
+    if (psiFile == null) {
+      showInfoMessage("No enclosing file found", project);
+      return;
+    }
+    PsiElement psiElement = psiFile.findElementAt(editor.getCaretModel().getOffset());
+    if (psiElement == null) {
+      showInfoMessage("No PSI for the element found", project);
+      return;
+    }
+    PsiElement methodPsiElement = findParentMethod(psiElement);
+    if (methodPsiElement == null) {
+      showInfoMessage("Current PSI element is not a part of any method", project);
+      return;
+    }
+    StringBuilder astSummaryBuilder = new StringBuilder();
+    traversePsiElement(methodPsiElement, astSummaryBuilder, 0);
+    showInfoMessage(astSummaryBuilder.toString(), project);
+  }
+
+  @Nullable
+  private static PsiElement findParentMethod(PsiElement psiElement) {
+    if (psiElement == null) {
+      return null;
+    }
+    return psiElement instanceof PsiMethodImpl
+        ? psiElement
+        : findParentMethod(psiElement.getParent());
   }
 
   private static void traversePsiElement(
       PsiElement psiElement,
-      StringBuilder stringBuilder,
+      StringBuilder astSummaryBuilder,
       int indentionLevel) {
     if (psiElement == null) {
       return;
     }
-    if (indentionLevel > 0) {
-      stringBuilder.append('\\');
-      stringBuilder.append(
-          Strings.repeat("-", indentionLevel * INDENTION_SPACES_PER_LEVEL - 1));
-    }
-    stringBuilder.append(psiElement.toString());
-    stringBuilder.append('\n');
+    astSummaryBuilder
+        .append(Strings.repeat("-", indentionLevel * INDENTION_SPACES_PER_LEVEL));
+    astSummaryBuilder.append(psiElement.toString());
+    astSummaryBuilder.append('\n');
     for (PsiElement element : psiElement.getChildren()) {
-      traversePsiElement(element, stringBuilder, indentionLevel + 1);
+      traversePsiElement(element, astSummaryBuilder, indentionLevel + 1);
     }
+  }
+
+  private static void showInfoMessage(String message, Project project) {
+    Messages.showMessageDialog(
+        project,
+        message,
+        "Information",
+        Messages.getInformationIcon());
   }
 }
